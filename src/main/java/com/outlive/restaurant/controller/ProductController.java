11e9a14b8308cfaacecb339f41ac15bed0c3618d
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-04-13 16:03:00 -0300

    Create the contract of get products endpoints

--- src/main/java/com/outlive/restaurant/controller/ProductController.java
+++ src/main/java/com/outlive/restaurant/controller/ProductController.java
@@ -2,15 +2,13 @@ package com.outlive.restaurant.controller;
 
 import com.outlive.restaurant.controller.dto.ProductRequest;
 import com.outlive.restaurant.controller.dto.ProductResponse;
-import com.outlive.restaurant.controller.dto.UserRequest;
-import com.outlive.restaurant.controller.dto.UserResponse;
 import com.outlive.restaurant.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +20,15 @@ public class ProductController {
     ResponseEntity<ProductResponse> create(@RequestBody ProductRequest productRequest) {
         final var productResponse = productService.create(productRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+
+    @GetMapping("/products")
+    ResponseEntity<List<ProductResponse>> getAllByQuery(@RequestParam String name) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users/{userId}/products/")
+    ResponseEntity<List<ProductResponse>> getAllBySeller(@PathVariable String userId) {
+        return ResponseEntity.ok().build();
     }
 }
