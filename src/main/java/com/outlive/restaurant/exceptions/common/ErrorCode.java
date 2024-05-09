84dac0d219f9d144c8c5c0f143a1deaa6e3ecf56
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-05-09 11:44:01 -0300

    Changing error code

--- src/main/java/com/outlive/restaurant/exceptions/common/ErrorCode.java
+++ src/main/java/com/outlive/restaurant/exceptions/common/ErrorCode.java
@@ -8,11 +8,18 @@ import lombok.Getter;
 public enum ErrorCode {
 
     USER_NOT_FOUND("USR_01"),
+
     SELLER_NOT_FOUND("SLR_01"),
-    CUSTOMER_NOT_FOUND("CUS_01"),
-    ADDRESS_NOT_FOUND("ADD_01");
+
+    CUSTOMER_NOT_FOUND("CST_01"),
+
+    ADDRESS_NOT_FOUND("ADS_01"),
+
+    //Products code error
+    PRODUCT_UNAVAILABLE("PRD_01"),
+    PRODUCT_OUT_STOCK("PRD_02"),
+    PRODUCT_PRICE_CHANGED("PRD_03");
 
     private final String code;
-
 
 }
