9d36c0d8730c71ce5daa845b8edbc2051262c565
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-08-03 09:56:41 -0300

    Add cors in the application

--- src/main/java/com/outlive/restaurant/OutliveRestaurantApplication.java
+++ src/main/java/com/outlive/restaurant/OutliveRestaurantApplication.java
@@ -2,10 +2,17 @@ package com.outlive.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class OutliveRestaurantApplication {
+@EnableWebMvc
+public class OutliveRestaurantApplication implements WebMvcConfigurer {
 
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OutliveRestaurantApplication.class, args);
 	}
